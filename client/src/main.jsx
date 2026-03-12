5ac20d2b9cb4ceda2bd454d8e52fd82e5c9b7ad5
snehalkhater <snehalkhater8@gmail.com>
2026-03-12 19:56:02 +0530

    components are added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,15 +5,21 @@ import "./index.css";
 import Signup from "././views/Auth/Signup";
 import Login from "././views/Auth/Login";
 import Dashboard from "./views/Users/Dashboard";
+import AdminDashboard from "./views/Admin/AdminDashboard";
+import UserNavbar from "./components/UserNavbar";
+import Footer from "./components/Footer";
 
 createRoot(document.getElementById("root")).render(
 
     <BrowserRouter>
+        <UserNavbar />
       <Routes>
+        <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+         <Route path="/admindashboard" element={<AdminDashboard />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
 
 );
---
+++ client/src/views/Admin/AdminDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdminDashboard() {
+  return (
+    <div>
+        <h2>Welcome to to Admin Dashboard</h2>
+    </div>
+  )
+}
+
+export default AdminDashboard
